f8c3bfaeef289d7dfbf3101b1902e55280d3617b
Alexander Brickwedde <bosch.brickwedde@extaccount.com>
2021-07-30 17:34:23 +0200

    First working version

---
+++ README.md
@@ -0,0 +1,27 @@
+# README
+
+Provides some helper functions to work with catkin projects.
+## Features
+1. Build catkin package of current file with dependencies.
+2. Build catkin package of current file w/o dependencies.
+3. Directly call make for the current file's package (requires initial `catkin build` of the package).
+4. Build tests for current package.
+5. Run all tests for current package (currently only works for `zsh`).
+6. TODO: Run all tests of current file (in case this includes tests).
+7. TODO: Run test under cursor in current file.
+8. TODO: Scan and visualize results of last test run.  
+
+## Keybindings
+| Name                                                             | Description       | Default Value |
+| ---------------------------------------------------------------- | ----------------- | ------------- |
+| `Catkin Helpers: catkin build current package`                   | See 1. from above | `undefined`   |
+| `Catkin Helpers: catkin build current package w/o dependencies.` | See 2. from above | `undefined`   |
+| `Catkin Helpers: Make current package`                           | See 3. from above | `ctrl+b`      |
+| `Catkin Helpers: Make tests of current package`                  | See 4. from above | `undefined`   |
+| `Catkin Helpers: Run tests of current package`                   | See 5. from above | `undefined`   |
+## Known Issues
+Nothing known so far. 
+
+**Please report more bugs! This is a very early version!**
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,92 @@
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+function getPackagePathOfFilename() {
+	const editor = vscode.window.activeTextEditor;
+	let packagename = "";
+	if (editor) {
+		const document = editor.document;
+		const filename = document.fileName;
+		const name_splitters = filename.split("/");
+		const src_index = name_splitters.lastIndexOf("src");
+		const header_index = name_splitters.lastIndexOf("include");
+		const test_index = name_splitters.lastIndexOf("test");
+		const index = Math.max(src_index, header_index, test_index);
+		if (index === -1) {
+			console.log("Could not find (src|include) in filename.");
+			vscode.window.showErrorMessage("Could not deduce package name from in full filename: " + filename);
+			return packagename;
+		}
+		packagename = name_splitters[index - 1];
+	}
+	return packagename;
+};
+
+function getTerminal() {
+	const terminals = vscode.window.terminals;
+	if (terminals.length === 0) {
+		const term = vscode.window.createTerminal();
+		const packagename = getPackagePathOfFilename();
+		term.show();
+		term.sendText("cd $(catkin locate -s " + packagename + ")\n");
+	}
+	return vscode.window.terminals[0];
+}
+
+function runCommandForFile(command: string) {
+	const terminal = getTerminal();
+	terminal.show();
+	terminal.sendText(command + "\n");
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Catkin helpers extension activated.');
+
+	let catkin_build_command = vscode.commands.registerCommand('catkin-helpers.catkin_build', () => {
+		const packagename = getPackagePathOfFilename();
+		if (packagename.length !== 0) {
+			const command = "catkin build " + packagename;
+			runCommandForFile(command);
+		}
+	});
+	context.subscriptions.push(catkin_build_command);
+
+	let catkin_build_no_deps_command = vscode.commands.registerCommand('catkin-helpers.catkin_build_no_deps', () => {
+		const packagename = getPackagePathOfFilename();
+		if (packagename.length !== 0) {
+			const command = "catkin build " + packagename + " --no-deps\n";
+			runCommandForFile(command);
+		}
+	});
+	context.subscriptions.push(catkin_build_no_deps_command);
+
+
+	let make_command = vscode.commands.registerCommand('catkin-helpers.make', () => {
+		const packagename = getPackagePathOfFilename();
+		if (packagename.length !== 0) {
+			const command = "make install -j20 -C $(catkin locate -b " + packagename + ")";
+			runCommandForFile(command);
+		}
+	});
+	context.subscriptions.push(make_command);
+
+	let make_tests_command = vscode.commands.registerCommand('catkin-helpers.make_tests', () => {
+		const packagename = getPackagePathOfFilename();
+		if (packagename.length !== 0) {
+			const command = "make tests -j20 -C $(catkin locate -b " + packagename + ")";
+			runCommandForFile(command);
+		}
+	});
+	context.subscriptions.push(make_tests_command);
+
+	let run_tests_command = vscode.commands.registerCommand('catkin-helpers.run_tests', () => {
+		const packagename = getPackagePathOfFilename();
+		if (packagename.length !== 0) {
+			const command = "source $(catkin locate -d)/setup.zsh && make test -C $(catkin locate -b " + packagename + ")";
+			runCommandForFile(command);
+		}
+	});
+	context.subscriptions.push(run_tests_command);
+}
+
+export function deactivate() { }
